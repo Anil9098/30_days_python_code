fe331e99ece36e02080d22cd5b70dc9f81eedbec
Anil9098 <90092212+Anil9098@users.noreply.github.com>
2022-01-05 11:38:21 +0530

    Add files via upload

---
+++ day1.1 task.py
@@ -0,0 +1,4 @@
+s1=input("enter a string \n")
+s2=input("enter a string \n")
+add = s1+s2
+print("addition of string",add)
---
+++ day1.2 task.py
@@ -0,0 +1,4 @@
+s1=str(input("enter first float \n"))
+s2=str(input("enter second float \n"))
+sum = s1+s2
+print("sum of two float",sum)
