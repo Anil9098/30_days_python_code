7e6ba2e051568c66516a45ebb693e5b6e6ec920d
Anil kumawat <90092212+Anil9098@users.noreply.github.com>
2022-01-07 10:26:40 +0530

    Add files via upload

---
+++ day 3.py
@@ -0,0 +1,18 @@
+#task 1
+#make a program  calculating for modulus,exponent,floor division
+a=int(input("enter a number \n"))
+b=int(input("enter a number \n"))
+print(f"reminder of {a} and {b} is {a%b}")
+print(f"exponent of {a} and {b} is {a**b}")
+print(f"floor division  of {a} and {b} is {a//b}")
+
+#task 2
+#make a program and use comparision operator
+a=int(input("enter a number \n"))
+b=int(input("enter a number \n"))
+print(f"Is a equal to b(True:False):{a==b}")
+print(f"Is a not equal to b(True:False):{a!=b}")
+print(f"Is a is greater than b(True:False):{a>b}")
+print(f"Is a is less than b(True:False):{a<b}")
+print(f"Is a is greater than equal to b(True:False):{a>=b}")
+print(f"Is a is less than equal to b(True:False):{a<=b}")
